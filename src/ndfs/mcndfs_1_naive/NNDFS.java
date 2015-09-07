1f2dd6945ffbbcc53ae2cd311c5b0a7d74b11920
Luc-Veldhuis <luc.veldhuis@nubisonline.nl>
2015-09-07 11:33:05 +0200

    Provided framework

---
+++ src/ndfs/mcndfs_1_naive/NNDFS.java
@@ -0,0 +1,78 @@
+package ndfs.mcndfs_1_naive;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import graph.Graph;
+import graph.GraphFactory;
+import graph.State;
+import ndfs.CycleFoundException;
+import ndfs.NDFS;
+import ndfs.NoCycleFoundException;
+import ndfs.ResultException;
+
+/**
+ * This is a straightforward implementation of Figure 1 of
+ * <a href="http://www.cs.vu.nl/~tcs/cm/ndfs/laarman.pdf">
+ * "the Laarman paper"</a>.
+ *
+ * This class should be modified/extended to implement Figure 2 of this paper.
+ */
+public class NNDFS implements NDFS {
+
+    private final Graph graph;
+    private final Colors colors = new Colors();
+
+    /**
+     * Constructs an NDFS object using the specified Promela file.
+     *
+     * @param promelaFile
+     *            the Promela file.
+     * @param nrWorkers
+     *            the number of worker threads to use.
+     * @throws FileNotFoundException
+     *             is thrown in case the file could not be read.
+     */
+    public NNDFS(File promelaFile, int nrWorkers) throws FileNotFoundException {
+
+        this.graph = GraphFactory.createGraph(promelaFile);
+    }
+
+    private void dfsRed(State s) throws ResultException {
+
+        for (State t : graph.post(s)) {
+            if (colors.hasColor(t, Color.CYAN)) {
+                throw new CycleFoundException();
+            } else if (colors.hasColor(t, Color.BLUE)) {
+                colors.color(t, Color.RED);
+                dfsRed(t);
+            }
+        }
+    }
+
+    private void dfsBlue(State s) throws ResultException {
+
+        colors.color(s, Color.CYAN);
+        for (State t : graph.post(s)) {
+            if (colors.hasColor(t, Color.WHITE)) {
+                dfsBlue(t);
+            }
+        }
+        if (s.isAccepting()) {
+            dfsRed(s);
+            colors.color(s, Color.RED);
+        } else {
+            colors.color(s, Color.BLUE);
+        }
+    }
+
+    private void nndfs(State s) throws ResultException {
+        dfsBlue(s);
+        throw new NoCycleFoundException();
+    }
+
+    @Override
+    public void ndfs() throws ResultException {
+        nndfs(graph.getInitialState());
+    }
+}
