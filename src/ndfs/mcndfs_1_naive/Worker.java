e51c07598489f6d93e95cca0091d56e5945b7d5e
Coenvu <czr700@vunet.vu.nl>
2015-09-19 14:55:22 +0200

    Pink global coloring addition
    paper 4.2 alg 2

--- src/ndfs/mcndfs_1_naive/Worker.java
+++ src/ndfs/mcndfs_1_naive/Worker.java
@@ -6,51 +6,60 @@ import graph.State;
 import ndfs.CycleFoundException;
 import ndfs.NoCycleFoundException;
 import ndfs.ResultException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 
 public class Worker implements Runnable {
 
+	
     private final Graph graph;
     private final Colors colors;
+    private static AtomicInteger redStateCounter;
+    private Colors pink; //ik gebruik maar Colors ipv nieuwe map met binaries
     private int threadId;
     private ResultArray resultArray;
     public Thread thread;
-
+    
+    
     public Worker(File promelaFile, Colors colors, int threadId, ResultArray resultArray) throws FileNotFoundException {
         this.graph = GraphFactory.createGraph(promelaFile);
         this.colors = colors;
         this.threadId = threadId;
         this.resultArray = resultArray;
+        redStateCounter = new AtomicInteger();
     }
 
     private void dfsRed(State s) throws ResultException {
-
+    	pink.color(s, Color.PINK);
         for (State t : graph.post(s)) {
             if (colors.hasColor(t, Color.CYAN)) {
                 throw new CycleFoundException();
-            } else if (colors.hasColor(t, Color.BLUE)) {
-                colors.color(t, Color.RED);
+            } else if (!colors.hasColor(t, Color.RED) && !pink.hasColor(t,Color.PINK)) {
                 dfsRed(t);
             }
         }
+        if(s.isAccepting()){
+        	redStateCounter.getAndDecrement();
+        	while (redStateCounter.get() != 0){}
+        }
+        colors.color(s,Color.RED);
+        pink.color(s, null); //mogelijk een probleem als compare methode niet met null om kan gaan, white / alternatieve kleur gebruiken?
     }
-
+    
     private void dfsBlue(State s) throws ResultException {
-
         colors.color(s, Color.CYAN);
         for (State t : graph.post(s)) {
-            if (colors.hasColor(t, Color.WHITE)) {
+            if (colors.hasColor(t, Color.WHITE) && !colors.hasColor(t,Color.RED)) {
                 dfsBlue(t);
             }
         }
         if (s.isAccepting()) {
-            dfsRed(s);
-            colors.color(s, Color.RED);
-        } else {
-            colors.color(s, Color.BLUE);
+        	redStateCounter.getAndIncrement();
+        	dfsRed(s);
         }
+        colors.color(s, Color.BLUE);
     }
 
     private void nndfs(State s) throws ResultException {
