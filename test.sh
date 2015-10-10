20d65b0e2a8fb2ac5f49c5d9b4f031dadd18db88
Luc Veldhuis <jfveldhuis@outlook.com>
2015-10-10 15:04:13 +0200

    Added loops for all files

--- test.sh
+++ test.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
-if [ "$#" -ne 4 ]; then
-        echo "Usage <inputfile> <implementation> <threads> <runs>" >&2
+if [ "$#" -ne 2 ]; then
+        echo "Usage <implementation> <runs>" >&2
         exit 1
 fi
 shortestTime=100000000000
 longestTime=0
 totalTime=0
-for i in `seq 1 $4`
+regex=" ([0-9]+) "
+files=input/*.prom
+for file in $files
 do
-        result=$(bin/ndfs $1 $2 $3)
-        [[ $result =~ ([0-9]{2,}) ]]
-        time=${BASH_REMATCH[1]}
-        echo "Run $i: $time  ms"
-        totalTime=$(( $time+$totalTime))
-        if [ "$time" -gt "$longestTime" ]; then
-                longestTime=$time
-        fi
-        if [ "$time" -lt "$shortestTime" ]; then
-                shortestTime=$time
-        fi
+        for j in 1 4 16 64
+        do
+                echo "Processing $file on $j threads"
+                for i in `seq 1 $2`
+                do
+                        result=$(bin/ndfs $file $1 $j)
+                        [[ $result =~ $regex ]]
+                        time=${BASH_REMATCH[1]}
+                        echo "Run $i: $time ms"
+                        totalTime=$(( $time+$totalTime))
+                        if [ "$time" -gt "$longestTime" ]; then
+                                longestTime=$time
+                        fi
+                        if [ "$time" -lt "$shortestTime" ]; then
+                                shortestTime=$time
+                        fi
+                done
+                averageTime=$(($totalTime/$2))
+                echo "File: $file on $j threads in $1"
+                echo "Average time $averageTime ms"
+                echo "Shortest time: $shortestTime ms"
+                echo "Longest time: $longestTime ms"
+        done
 done
-averageTime=$(($totalTime/$4))
-echo "Average time $averageTime ms"
-echo "Shortest time: $shortestTime ms"
-echo "Longest time: $longestTime ms"
